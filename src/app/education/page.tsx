fdec86c53c12d2acdaff77be36c94a73c248537f
Kinotijoan <kinotijoan@gmail.com>
2024-07-16 18:49:38 +0300

    chore: Add puppeteer dependency for web scraping

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import Ed_card from './Ed_card'
+"use client"
+import React, { useEffect, useState } from "react";
+import Ed_card from "./Ed_card";
+import { scrapeBlogs } from "@/webScraping/ScrapeForge";
 
-const page = () => {
-  return (
-    <Ed_card />
-  )
-}
+type Blog = {
+  title: string;
+  description: string;
+  author: string;
+  date: string;
+  image: string;
+  link: string;
+};
 
-export default page
+const Page: React.FC = () => {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const blogsData = await scrapeBlogs(
+        "https://www.forgerecycling.co.uk/blog/category/recycling/page/2/"
+      ).then();
+      if (blogsData) {
+        setBlogs(blogsData);
+      }
+    };
+
+    fetchBlogs();
+    close
+  }, []);
+  return <Ed_card />;
+};
+
+export default Page;
