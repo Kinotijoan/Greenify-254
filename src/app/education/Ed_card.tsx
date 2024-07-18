a1690b296eb1549e82672433c9f649d1dc6aaa0b
Kinotijoan <kinotijoan@gmail.com>
2024-07-18 17:41:20 +0300

    chore: Update next.config.mjs and Ed_card.tsx

--- src/app/education/Ed_card.tsx
+++ src/app/education/Ed_card.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Calendar, BookOpen, ArrowUpRightFromSquare } from "lucide-react";
 import { Button } from "@/components/UI/Button";
 import Link from "next/link";
+import Image from "next/image";
+ 
 
 
 interface Ed_cardProps {
@@ -22,38 +24,31 @@ const Ed_card = ({
   link,
 }: Ed_cardProps) => {
   const defaultImageUrl =
-    "../../public/data_scientist.jpeg"
+    "/images/3R's.png";
   return (
-    <div className="w-1/5 m-2 border-2 border-green-800 rounded-lg shadow-black space-y-4 p-4">
-      <h3 className="font-bold text-xl">{title}</h3>
-      <div className=" flex space-x-6">
-        <div className="flex flex-row items-center space-x-2">
-          <Calendar size={20} />
-          <span>{date}</span>
-        </div>
-        <div className="flex flex-row items-center space-x-2 ">
-          <BookOpen size={20} />
-          {author ? <span>{author}</span> : <span>Anonymous</span>}
-        </div>
-      </div>
-      <div
-        className=" w-10/12 rounded-lg "
-        style={{
-          backgroundImage: `url(${img_url ? img_url : defaultImageUrl})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      ></div>
-      <p className="font-light">
-        {description}.
-      </p>
-      <Link href={link}>
-      <Button variant="outline" className="ml-44">
-        Read more
-        <ArrowUpRightFromSquare className="ml-2  h-4 w-4" />
-      </Button>
-      </Link>
+   <div className="box space-y-3">
+  <h3 className="font-bold text-xl">{title}</h3>
+  <div className="flex space-x-6">
+    <div className="flex flex-row items-center space-x-2">
+      <Calendar size={20} />
+      <span>{date}</span>
     </div>
+    <div className="flex flex-row items-center space-x-2">
+      <BookOpen size={20} />
+      {author ? <span>{author}</span> : <span>Anonymous</span>}
+    </div>
+  </div>
+
+  {img_url ? <Image src={img_url} alt={title} width={300} height={100} className="rounded-xl" /> : <Image src={defaultImageUrl} alt={title} width={200} height={150} />}
+  
+  <p className="font-light">{description}.</p>
+  <Link href={link}>
+    <Button variant="outline" className="ml-auto">
+      Read more
+      <ArrowUpRightFromSquare className="ml-2 h-4 w-4" />
+    </Button>
+  </Link>
+</div>
   );
 };
 
