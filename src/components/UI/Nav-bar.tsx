3fbe00ab7a90072f24073b900c5dbf43a8b1ef24
Sandrah <comfortsandrah13@gmail.com>
2024-07-18 15:51:23 +0300

    chore: Update Nav-bar component to use green color for hover text

--- src/components/UI/Nav-bar.tsx
+++ src/components/UI/Nav-bar.tsx
@@ -14,7 +14,7 @@ const NavLink = ({ links }: NavProps) => {
   return (
     <ul className='flex text-xl lg:w-96 gap-2  justify-between pl-5 pt-4'>
         {links.map((link, index) => (
-            <li key={index} className="hover:text-purple-500">
+            <li key={index} className="hover:text-green-500">
                 <Link href={link.href}>
                 {link.text}
                 </Link>
