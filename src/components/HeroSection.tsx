d50fd9c0f29ccb00a3ccb36afea3d5850512b64f
Sandrah <comfortsandrah13@gmail.com>
2024-07-19 10:45:10 +0300

    refactor: Update HeroSection component layout and fix link in Learn more button

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,30 +10,34 @@ const Hero = () => {
       <div>
         <Header />
       </div>
-      <div className="h-[90vh] hero-bg w-full absolute"></div>
+      <div className="h-[105vh] hero-bg w-full absolute"></div>
       <div className=" flex justify-evenly h-full relative">
         <Image
           src="/world.png"
           alt="world"
           width={350}
           height={350}
-          className="hidden md:block absolute left-20 -top-10"
+          className="hidden md:block absolute left-5 top-0"
         />
         <Image
           src="/recycle.png"
           alt="recycle"
           width={250}
           height={250}
-          className="hidden md:block absolute right-20 -top-7 "
+          className="hidden md:block absolute right-20 top-8"
         />
-        <div className="  flex flex-col items-center justify-center p-20">
-          <h1 className=" font-bold text-6xl">Turning Trash Into Treasure</h1>
-          <p className=" text-center text-2xl mt-3 max-w-[860px]">
-          Make a difference for our planet, one step at a time. Wasteless empowers you to adopt sustainable habits and create a positive impact on the environment.
+        <div className="  flex flex-col items-center my-auto md:justify-center px-5 select-none ">
+          <h1 className=" font-bold text-5xl mb-6 text-center md:text-6xl ">
+            Turning Trash Into Treasure
+          </h1>
+          <p className=" text-center text-2xl mb-16 max-w-[860px]">
+            Make a difference for our planet, one step at a time. Wasteless
+            empowers you to adopt sustainable habits and create a positive
+            impact on the environment.
           </p>
           <Button
             asChild
-            className=" text-black bg-white rounded-full mt-3 text-xl hover:bg-green-500 hover:text-white"
+            className=" text-black bg-white rounded-full  text-xl hover:bg-green-500 hover:text-white"
           >
             <Link href="/#about-us">Learn more</Link>
           </Button>
