7d54ce0c5396034ccd7dce9143310ab4d8531468
FaithMosonik <mosonikchep@gmail.com>
2024-07-19 11:57:47 +0300

    Changed the icons in the section for companies

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -11,41 +11,41 @@ const CompaniesSection = () => {
         </h2>
       </div>
       <div className="flex justify-evenly text-center">
-        <div className="mx-20">
+        <div className="mx-10">
           <div className="flex justify-center">
-            <Image src="/images/PostIcon.png" alt="" width={200} height={200} />
+            <Image src="/trash.png" alt="" width={200} height={200} />
           </div>
           <div>
             <h3 className="font-medium text-[#FFB84D] text-2xl">
-              Post what you're recycling
+              Market recycled products
             </h3>
-            <p className="text-xl">
+            <p className="">
             Increase awareness of your recycling services! Post the materials you accept on our app, attracting new customers and promoting responsible waste management practices. Grow your business and contribute to a circular economy.
             </p>
           </div>
         </div>
-        <div className="mx-20">
-          <div className="flex justify-center">
-            <Image src="/images/search.png" alt="" width={200} height={200} />
+        <div className="mx-10">
+          <div className="flex justify-center mb-10">
+            <Image src="/images/profile.jpg" alt="" width={200} height={200} />
           </div>
           <div>
             <h3 className="font-medium text-[#FFB84D] text-2xl">
               Update your profile
             </h3>
-            <p className="text-xl">
+            <p className="">
             Keep your company profile up-to-date! Easily edit your contact details, service offerings, and location information, ensuring users can find you effortlessly. Manage your online presence and connect with eco-conscious customers.
             </p>
           </div>
         </div>
-        <div className="mx-20">
+        <div className="mx-10">
           <div className="flex justify-center">
-            <Image src="/images/event.png" alt="" width={200} height={200} />
+            <Image src="/images/event-sketch.png" alt="" width={200} height={200} />
           </div>
           <div>
             <h3 className="font-medium text-[#FFB84D] text-2xl">
               Post an event
             </h3>
-            <p className="text-xl">
+            <p className="">
             Educate and engage your community!  Announce upcoming recycling drives, composting workshops, or other sustainability initiatives through our event posting feature.  Reach new customers, raise awareness of your services, and foster a collaborative approach to waste reduction.
             </p>
           </div>
