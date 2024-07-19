27020fd49f780220065fd38177528272ea5d0da6
Sandrah <comfortsandrah13@gmail.com>
2024-07-19 10:56:40 +0300

    refactor: Update Nav-bar component to use client-side rendering and improve link handling

--- src/components/UI/Nav-bar.tsx
+++ src/components/UI/Nav-bar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 
 export interface NavLink {
@@ -9,19 +10,34 @@ export interface NavLink {
 interface NavProps {
   links: NavLink[];
 }
+const links: NavLink[] = [
+  { text: "Home", href: "#" },
+  { text: "Feature", href: "/#features" },
+  { text: "About us", href: "/#about-us" },
+  { text: "How it works", href: "/#how-it-works" },
+  { text: "Contact", href: "/#footer" },
+];
 
-const NavLink = ({ links }: NavProps) => {
+const NavLink = () => {
+  const [currentLink, setCurrentLink] = useState("Home");
   return (
-    <ul className='flex text-xl font-medium gap-8 justify-between pt-4'>
-        {links.map((link, index) => (
-            <li key={index} className="hover:text-green-500">
-                <Link href={link.href}>
-                {link.text}
-                </Link>
-            </li>
-        ))}
+    <ul className="flex text-xl font-medium gap-8 justify-between">
+      {links.map(({text,href}, index) => (
+        <li
+          key={index}
+          className="hover:text-green-500"
+          onClick={() => setCurrentLink(text)}
+        >
+          <Link
+            href={href}
+            className={`${text === currentLink ? "text-green-500" : ""}`}
+          >
+            {text}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
-}
+};
 
 export default NavLink;
