394b7c699aec297ef5e52f4ec56207f024b7147c
Kinotijoan <kinotijoan@gmail.com>
2024-07-20 23:18:45 +0300

    refactor: Update package-lock.json with latest dependencies

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -39,9 +39,14 @@ const Features = () => {
           height={250}
           className="hidden md:block md:left-0 select-none "
         />
-        <div className="flex flex-col md:flex-row items-center  gap-5 md:gap-10 w-full mt-3">
+        <div className="flex flex-col md:flex-row items-center md:justify-center gap-5 md:gap-10 w-full mt-3">
           {myFeatures.map(({ title, description, href }, index) => (
-            <Card title={title} description={description} href={href} />
+            <Card
+              key={index}
+              title={title}
+              description={description}
+              href={href}
+            />
           ))}
         </div>
       </div>
