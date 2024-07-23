20aebe5cc41c3b08a54c552f2b334cd9a9512bff
Sandrah <comfortsandrah13@gmail.com>
2024-07-23 09:55:35 +0300

    refactor: Update login button font weight for better visibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
         <h1 className="text-3xl md:text-4xl select-none">Wasteless</h1>
         <div className=" flex space-x-2 md:space-x-6 justify-between items-center">
           <NavLinks />
-          <Button className="bg-transparent hover:bg-green-500 hover:text-white hover:outline-1 text-green-500 ring-1 ring-green-500 rounded-full">
+          <Button className="bg-transparent hover:bg-green-500 hover:text-white hover:outline-1 text-green-500 ring-1 ring-green-500 font-extrabold rounded-full">
             Login
           </Button>
         </div>
