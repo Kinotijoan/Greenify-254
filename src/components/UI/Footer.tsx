212a9e14559fdeb5e6f5fdd8c3c571156aa7da8b
Sandrah <comfortsandrah13@gmail.com>
2024-07-24 21:27:39 +0300

    refactor: Update Footer component layout and styling for better responsiveness

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { NavLink } from "./Nav-bar";
 import { url } from "inspector";
+import { Button } from "./Button";
+import { Mail } from "lucide-react";
 
 const Footer: React.FC = () => {
   const links: NavLink[] = [
@@ -29,7 +31,10 @@ const Footer: React.FC = () => {
       className="bg-cover bg-no-repeat bg-center bottom-0 w-full"
       style={{ backgroundImage: `url("/images/Rectangle.jpg")` }}
     >
-      <div className="bg-opacity-75 text-white h-full flex flex-col container justify-between">
+      <section
+        className="bg-opacity-75 text-white h-full flex flex-col container justify-between"
+        id="footer"
+      >
         <h3 className="text-2xl font-bold text-center my-3">WASTELESS</h3>
         <div className="container flex flex-col items-start sm:flex-row sm:justify-between gap-3 mt-6">
           <div className="mb-6 sm:mb-0">
@@ -37,15 +42,15 @@ const Footer: React.FC = () => {
             <ul className="">
               {links.map((link, index) => (
                 <li key={index} className="mb-2 hover:scale-105 ">
-                  <Link className="font-normal text-lg" href={link.href}>{link.text}</Link>
+                  <Link className="font-normal text-lg" href={link.href}>
+                    {link.text}
+                  </Link>
                 </li>
               ))}
             </ul>
           </div>
           <div className="mb-6">
-            <h2 className="text-xl font-semibold mb-4" id="footer">
-              Reach out to us
-            </h2>
+            <h2 className="text-xl font-semibold mb-4">Reach out to us</h2>
             {/* <div className="hover:scale-105 transition-transform duration-300 flex items-center mb-4 font-normal text-sm ">
               <Image
                 src="/icons/github(1).png"
@@ -81,10 +86,7 @@ const Footer: React.FC = () => {
                 height={25}
                 className="mr-2"
               />
-              <a
-                href="mailto:wasteless2024@gmail.com"
-                className="text-lg"
-              >
+              <a href="mailto:wasteless2024@gmail.com" className="text-lg">
                 wasteless2024@gmail.com
               </a>
             </div>
@@ -94,18 +96,23 @@ const Footer: React.FC = () => {
             <input
               type="text"
               placeholder="Your Name"
-              // max={1}
+              maxLength={50}
               className="bg-transparent border-b border-white text-white mb-4 w-full focus:outline-none  md:text-lg"
             />
-             <input
+            <input
               type="email"
               placeholder="Your Email Address"
               className="bg-transparent border-b border-white text-white mb-4 w-full focus:outline-none  md:text-lg"
             />
             <textarea
               placeholder="Your Message"
-              className="bg-transparent border-b border-white text-white h-20 w-full focus:outline-none  md:text-lg"
+              maxLength={200}
+              rows={3}
+              className="bg-transparent border-b border-white text-white  w-full focus:outline-none  md:text-lg "
             ></textarea>
+            <Button className="bg-green-500 text-black rounded-full flex mx-auto hover:text-white hover:bg-transparent ring-green-500 hover:ring mt-3">
+              Submit
+            </Button>
           </div>
         </div>
         <div className="flex items-center justify-center py-10">
@@ -121,7 +128,7 @@ const Footer: React.FC = () => {
             <p className="md:text-lg ">2024 All rights reserved</p>
           </div>
         </div>
-      </div>
+      </section>
     </footer>
   );
 };
