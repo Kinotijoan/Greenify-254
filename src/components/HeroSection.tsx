1e50e7bfba0f9794e5d9f29fe2495d8e84b6fd5d
Sandrah <comfortsandrah13@gmail.com>
2024-07-31 21:18:59 +0300

    chore: Update import statement for Header component in HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./header";
+import Header from "./Header";
 import Link from "next/link";
 import { Button } from "./UI/Button";
 import Image from "next/image";
