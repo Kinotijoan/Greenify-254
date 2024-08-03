d2f19d0ff832be76cf83a68c003989cba2067873
Kinotijoan <kinotijoan@gmail.com>
2024-08-03 18:54:35 +0300

    chore: Add PasswordResetToken table and remove email column

---
+++ prisma/migrations/20240802051248_password_reset_token/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "PasswordResetToken" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PasswordResetToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "PasswordResetToken_userId_idx" ON "PasswordResetToken"("userId");
+
+-- AddForeignKey
+ALTER TABLE "PasswordResetToken" ADD CONSTRAINT "PasswordResetToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Individual"("individualId") ON DELETE RESTRICT ON UPDATE CASCADE;
