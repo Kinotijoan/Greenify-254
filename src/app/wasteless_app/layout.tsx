a16f1c58bc44f6a0d9895b782c976695b5ed987e
MinayoMelanie <melanieminayo00@gmail.com>
2024-08-03 17:28:32 +0300

    added layout on app

---
+++ src/app/wasteless_app/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from "./components/sidebar";
+import Navbar from "./components/navbar";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="flex flex-col">
+      <Navbar />
+      <div className="flex flex-row h-full">
+        <Sidebar />
+        <div className="flex-1"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
