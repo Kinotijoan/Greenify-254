dc2c8ac85f065777ecf025ca530bc436e87a7b46
Kinotijoan <kinotijoan@gmail.com>
2024-08-20 16:47:41 +0300

    chore: Add functions for password reset and email verification

---
+++ src/app/api/forgotPassword/functions.tsx
@@ -0,0 +1,28 @@
+import { TimeSpan, createDate } from "oslo";
+import { sha256 } from "oslo/crypto";
+import {encodeHex} from "oslo/encoding";
+import { generateIdFromEntropySize } from "lucia";
+import prisma from "@/lib/prisma";
+
+export async function createPasswordResetToken(userId: string){
+
+    await prisma.passwordResetToken.deleteMany({
+        where: {
+            accountId: userId,
+        },
+    });
+
+    const tokenId = generateIdFromEntropySize(16);
+    const token = encodeHex(await sha256(new TextEncoder().encode(tokenId)));
+  
+    await prisma.passwordResetToken.create({
+        data: {
+            accountId: userId,
+            token: token,
+            expiresAt: createDate(new TimeSpan(2, "h")),
+        },
+    });
+
+    return token;
+
+}
