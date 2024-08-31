2294988a40eda1afde7629bdf22d77e284b50ab2
Sandrah <comfortsandrah13@gmail.com>
2024-08-31 13:15:20 +0300

    Update on authentication

--- src/app/wasteless_app/layout.tsx
+++ src/app/wasteless_app/layout.tsx
@@ -8,45 +8,26 @@ interface LayoutProps {
 
 export default async function Layout({ children }: LayoutProps) {
   const result = await validateRequest();
+
   if (!result) {
-    // redirect("/login");
-    return (
-      <div className="flex bg-greenbg h-screen overflow-hidden">
-        {/* <Sidebar user={user} /> */}
-        <Sidebar />
-
-        <div className="flex-grow bg-white rounded-lg p-4 m-1 md:ml-0 overscroll-contain overflow-auto">
-          {children}
-        </div>
-      </div>
-    );
+    redirect("/login");
+    return null; // No JSX returned since the redirect will handle navigation.
   }
-  // const { user } = result;
 
+  const { user } = result; // Uncomment if user data is needed
 
-  
-  // const {user} = await validateRequest();
+  if(!user) {
+    redirect("/login");
+  }
 
-  // if (!user) {
-  //   // redirect("/login");
-  //   return (
-  //     <div className="flex bg-greenbg h-screen overflow-hidden">
-  //       <Sidebar user={user} />
+  return (
+    <div className="flex bg-greenbg h-screen overflow-hidden">
+      {/* Pass user to Sidebar if needed: <Sidebar user={user} /> */}
+      <Sidebar  user={user}/>
 
-  //       <div className="flex-grow bg-white rounded-lg p-4 m-1 md:ml-0 overscroll-contain overflow-auto">
-  //         {children}
-  //       </div>
-  //     </div>
-  //   );
-  // }
-  //  return (
-  //    <div className="flex bg-greenbg h-screen overflow-hidden">
-  //      <Sidebar user={user} />
-
-  //      <div className="flex-grow bg-white rounded-lg p-4 m-1 md:ml-0 overscroll-contain overflow-auto">
-  //        {children}
-  //      </div>
-  //    </div>
-  //  );
-
+      <div className="flex-grow bg-white rounded-lg p-4 m-1 md:ml-0 overscroll-contain overflow-auto">
+        {children}
+      </div>
+    </div>
+  );
 }
